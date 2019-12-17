77ce7ef24b12403c983a2eb090d3508b00e7d9a9
Ukachuku <57579782+Ukachuku@users.noreply.github.com>
2019-12-16 20:38:29 -0800

    Update cardenas.py

--- R/cardenas.py
+++ R/cardenas.py
@@ -2,17 +2,19 @@ import pandas as pd
 import os
 import re
 
+#function to mimic excel right function
 def right(s, amount):
     return s[-amount:]
 
-
 os.chdir('\home\pi\OCR')
 
+#read text file from tesseract OCR in r
 file = open('output.txt', mode='r')
 #replace the newline char with space. simplify the reg expression process
 data = file.read().replace('\n', ' ')
+#split into list from pattern on reciept. every transaction ends in 'N F' or 'T'
 data = re.split(' T |N F', data)
-
+#regex to extract prices and append to list
 prices = []
 for i in range(len(data)):
     price = right(data[i], 7)
@@ -20,4 +22,5 @@ for i in range(len(data)):
     price = price[price.find('$'):]
     price = price.strip()
     price = price.replace(' ', '.')
+    price = price.replace('$', '')
     prices.append(price)
